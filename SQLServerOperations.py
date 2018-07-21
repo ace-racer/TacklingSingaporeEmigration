63a5de77ebd60e2beafa6b1ad5d431b639a50415
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-07-22 00:53:18 +0800

    Add code to add recent records for any country only

--- SQLServerOperations.py
+++ SQLServerOperations.py
@@ -24,15 +24,11 @@ class SQLServerOperations(BaseOperations):
                 print('row = %r' % (row,))
 
     
-    def insert_new_records(self, insert_queries, table_name):
+    def insert_new_records(self, insert_queries):
         if self.configurations is not None and insert_queries is not None:
             try:
                 cnxn = pyodbc.connect(self.configurations["target_connection_string"])
                 cursor = cnxn.cursor()
-
-                cursor.execute("SELECT year, month from {0} order by year desc, month desc".format(table_name))
-                for row in cursor:
-                    print('row = %r' % (row,))
 
                 for insert_query in insert_queries:
                     cursor.execute(insert_query)
@@ -46,34 +42,69 @@ class SQLServerOperations(BaseOperations):
     def insert_required_records(self, spreadsheet_operations):
         if spreadsheet_operations is not None and self.configurations is not None:
             
-            table_name = self.configurations["table_name"]
-            input_df = spreadsheet_operations.read_all_rows_from_sheet()
-            column_names = spreadsheet_operations.get_sheet_headers()
-            # print(column_names)
+            try:
+                table_name = self.configurations["table_name"]
+                input_df = spreadsheet_operations.read_all_rows_from_sheet()
+                column_names = spreadsheet_operations.get_sheet_headers()
+                # print(column_names)
 
-            insert_query_columns = ",".join(column_names)
+                cnxn = pyodbc.connect(self.configurations["target_connection_string"])
+                cursor = cnxn.cursor()
 
-            # filter the rows by the year and the month
-            if input_df is not None:
-                insert_queries = []
-                for _, row in input_df.iterrows():
-                    query_values = []
-                    for column_idx in range(len(column_names)):
-                        query_value = str(row[column_names[column_idx]])
-                        if self.compiled_pattern_for_real_values.match(query_value):
-                            query_values.append(query_value)
-                        else:
-                            query_values.append("'{0}'".format(query_value))
-                    
-                    query_values_appended = ",".join(query_values)
-                    insert_query = self.insert_query_template.format(table_name, insert_query_columns, query_values_appended)
-                    print("Query: " + insert_query)
-                    insert_queries.append(insert_query)
+                cursor.execute("SELECT top(1) year, month from {0} order by year desc, month desc".format(table_name))
+                first_row = cursor.fetchall()[0]
+                latest_year = int(first_row[0])
+                latest_month = int(first_row[1])
+                latest_date_from_table = (latest_year, latest_month)
+                print("Latest year {0} and latest month {1} already in table.".format(latest_year, latest_month))
 
-                self.insert_new_records(insert_queries, table_name)
 
-            else:
-                print("Input dataframe is not properly read.")
+                insert_query_columns = ",".join(column_names)
+
+                # filter the rows by the year and the month
+                if input_df is not None:
+                    insert_queries = []
+                    for _, row in input_df.iterrows():
+                        query_values = []
+                        for column_idx in range(len(column_names)):
+                            query_value = str(row[column_names[column_idx]])
+                            if self.compiled_pattern_for_real_values.match(query_value):
+                                query_values.append(query_value)
+                                if column_names[column_idx].lower() == "year":
+                                    current_record_year = int(query_value)
+                                
+                                if column_names[column_idx].lower() == "month":
+                                    current_record_month = int(query_value)
+                            else:
+                                query_values.append("'{0}'".format(query_value))
+                        
+                        query_values_appended = ",".join(query_values)
+                        insert_query = self.insert_query_template.format(table_name, insert_query_columns, query_values_appended)
+                        
+                        current_record_date = (current_record_year, current_record_month)
+                        if self._should_insert_current_record(latest_date_from_table, current_record_date):
+                            print("Query: " + insert_query)
+                            insert_queries.append(insert_query)
+
+                    self.insert_new_records(insert_queries)
+            except Exception as ex:
+                print("An error occurred. Details: " + str(ex))
+        else:
+            print("Input dataframe is not properly read.")
+
+    def _should_insert_current_record(self, latest_from_table, current_from_source):
+        """
+           date format as year, month
+        """
+        if current_from_source[0] > latest_from_table[0]:
+            return True
+        elif current_from_source[0] == latest_from_table[0]:
+            if current_from_source[1] > latest_from_table[1]:
+                return True
+        
+        return False
+
+
 
 
 
