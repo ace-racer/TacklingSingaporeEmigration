36136876791a4d400ded783f344597a6b7f8118c
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-07-25 20:34:21 +0800

    Add data for GDP per capita PPP

--- sql_scripts/create_records.py
+++ sql_scripts/create_records.py
@@ -3,7 +3,7 @@ import numpy as np
 
 final_year = 2017
 
-df = pd.read_csv("unemployments_1.csv", encoding = "ISO-8859-1")
+df = pd.read_csv("../data/gdp_pc_ppp_usa_uk.csv", encoding = "ISO-8859-1")
 headers = df.columns.values
 print(headers)
 queries = ""
@@ -11,11 +11,11 @@ for idx, row in df.iterrows():
     if row["CountryID"] and not pd.isnull(row["CountryID"]):
         for itr in range(final_year - 2000 + 1):
             countryid = str(int(row["CountryID"]))
-            column_name = str(itr + 1)
-            value = (row[column_name])
+            date_id = str(itr + 1)
+            value = (row[date_id])
             if value and not pd.isnull(value):
                 value = "{:0.3f}".format(value)
-                queries += "insert into dimeconomy(unemploymentrate, countryid, timeid) values({0}, {1}, {2});\n".format(value, countryid, column_name)
+                queries += "update dimeconomy set purchasingpowerindex = {0} where countryid = {1} and timeid = {2};\n".format(value, countryid, date_id)
 
-with open("queries.sql", "w") as fw:
+with open("ppp_queries.sql", "w") as fw:
     fw.writelines(queries)
