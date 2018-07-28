298ac2a796796444814d0984e9ceacfe62005e34
Pier <madstrum@gmail.com>
2018-07-28 23:35:58 +0800

    Added emigration numbers script.

--- sql_scripts/update_fact_master_emig.py
+++ sql_scripts/update_fact_master_emig.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
+country_num = 198
 query = ""
-for itr in range(9):
+for itr in range(country_num):
     countryid = str(itr + 1)
     for jtr in range(18):
         timeid = str(jtr + 1)
-        query += "update factmaster set economyid = (select EconomyID from DimEconomy where CountryID = {0} and timeid = {1})".format(
+        query += "update factmaster set MigrationID = (select MigrationID from DimMigration where CountryID = {0} and timeid = {1})".format(
             countryid, timeid) + "where CountryID = {0} and TimeID = {1}; \n".format(countryid, timeid)
 
-with open("update_fact_master.sql", "w") as fw:
+with open("update_fact_master_emig.sql", "w") as fw:
     fw.writelines(query)
