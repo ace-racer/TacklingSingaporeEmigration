b023c749c7c754b21470cef965dc1c132e24e2fe
David Leonardi <davidleonardi418@gmail.com>
2018-08-04 17:39:45 +0800

    CHANGE: Change Happiness Index Data.

--- sql_scripts/create_records_happiness.py
+++ sql_scripts/create_records_happiness.py
@@ -1,32 +1,37 @@
 import pandas as pd
-
-final_year = 2017
+import math
 
 csv_files = [
-    "../data/un_world_happiness_report_index_2015.csv",
-    "../data/un_world_happiness_report_index_2017.csv",
-    "../data/un_world_happiness_report_index_2018.csv"
+    "../data/WHR2018Chapter2OnlineData.csv"
 ]
 
 queries_insert_detail = ""
 queries_fact_master = ""
+
+
+def valid_numeric(value):
+    if value and not math.isnan(value):
+        return str(value)
+    else:
+        return 0
+
 
 for csv_file in csv_files:
     df = pd.read_csv(csv_file, encoding="ISO-8859-1")
     headers = df.columns.values
     print(headers)
     for idx, row in df.iterrows():
-        if row["Country ID"] and not pd.isnull(row["Country ID"]):
-            country_id = str(int(row["Country ID"]))
-            time_id = str(int(row["Time ID"]))
-            happiness_index = str(row["Score"])
-            gdp_per_capita = str(row["GDP per capita"])
-            social_support = str(row["Social support"])
-            healthy_life = str(row["Healthy life expectancy"])
-            freedom = str(row["Freedom to make life choices"])
-            generosity = str(row["Generosity"])
-            trust = str(row["Trust"])
-            print([country_id, time_id, happiness_index, gdp_per_capita, social_support, healthy_life, freedom])
+        if row["Country ID"] and not pd.isnull(row["Country ID"]) and row["Happines Index"] > 0:
+            country_id = valid_numeric(row["Country ID"])
+            time_id = valid_numeric(row["Time ID"])
+            happiness_index = valid_numeric(row["Happines Index"])
+            gdp_per_capita = valid_numeric(row["Log GDP per capita"])
+            social_support = valid_numeric(row["Social support"])
+            healthy_life = valid_numeric(row["Healthy life expectancy at birth"])
+            freedom = valid_numeric(row["Freedom to make life choices"])
+            generosity = valid_numeric(row["Generosity"])
+            trust = valid_numeric(row["Perceptions of corruption"])
+            print([country_id, time_id, happiness_index, gdp_per_capita, social_support, healthy_life, freedom, generosity, trust])
             queries_insert_detail += "INSERT INTO DimHappiness " \
                                      "(countryid, timeid, HappinessIndex, " \
                                      "GDPPerCapita, SocialSupportIndex, HealthLifeExpectancyIndex, " \
