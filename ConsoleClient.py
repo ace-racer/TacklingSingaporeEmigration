1d6c241a3b51155a52a85aa30d760eba7937c969
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-08-06 20:22:11 +0800

    Add code tp update fact master after update to dimension table

--- ConsoleClient.py
+++ ConsoleClient.py
@@ -4,6 +4,4 @@ from SpreadsheetOperations import SpreadsheetOperations
 if __name__ == "__main__":
     sql_server_operations = SQLServerOperations()
     spreadsheet_operations = SpreadsheetOperations()
-    # sql_server_operations.read_all_records("GDPIndex")
     sql_server_operations.insert_required_records(spreadsheet_operations)
-    print("Records inserted successfully.")
--- SQLServerOperations.py
+++ SQLServerOperations.py
@@ -24,17 +24,35 @@ class SQLServerOperations(BaseOperations):
                 print('row = %r' % (row,))
 
     
-    def insert_new_records(self, insert_queries):
+    def insert_new_records(self, insert_queries, table_name):
         if self.configurations is not None and insert_queries is not None:
             try:
                 cnxn = pyodbc.connect(self.configurations["target_connection_string"])
+                key_in_fact_master = self.configurations["key_in_fact_master"]
                 cursor = cnxn.cursor()
 
                 for insert_query in insert_queries:
-                    cursor.execute(insert_query)
-                    print("Executed [{0}] successfully.".format(insert_query))  
+                    print(insert_query)
+                    if insert_query[1] and insert_query[2]:
+                        # insert into the dimension table
+                        cursor.execute(insert_query[0])
+                        cnxn.commit() 
+                        print("Executed [{0}] successfully.".format(insert_query[0]))  
 
-                cnxn.commit()     
+                        timeid = insert_query[1]
+                        countryid = insert_query[2]
+
+                        cursor.execute("Select ident_current('{0}')".format(table_name))
+                        fetch_result = cursor.fetchall()
+                        if fetch_result:
+                            first_row = fetch_result[0]
+                            id_value = first_row[0]
+                            print("ID: " + str(id_value))
+                            fact_master_query = "insert into factmaster (countryid, timeid, {0}) values({1}, {2}, {3})".format(key_in_fact_master, countryid, timeid, id_value)
+                            print("Trying to execute [{0}].".format(fact_master_query))
+                            cursor.execute(fact_master_query)
+                            print("Executed [{0}] successfully.".format(fact_master_query))
+                            cnxn.commit()     
             except Exception as e:
                 print("Error occurred while inserting record. Details: " + str(e))
                 return None
@@ -57,18 +75,19 @@ class SQLServerOperations(BaseOperations):
                     insert_queries = []
                     for _, row in input_df.iterrows():
                         query_values = []
+                        timeid = None
+                        countryid = None
                         for column_idx in range(len(column_names)):
-                            timeid = None
-                            countryid = None
+                           
                             query_value = str(row[column_names[column_idx]])
                             
                             # get the value of the time id
-                            if column_names[column_idx] == self.TIMEID_STR:
-                                timeid = query_value
+                            if column_names[column_idx].lower() == self.TIMEID_STR:
+                                timeid = int(float(query_value))
                             
                             # get the value of the country id
-                            if column_names[column_idx] == self.COUNTRYID_STR:
-                                countryid = query_value
+                            if column_names[column_idx].lower() == self.COUNTRYID_STR:
+                                countryid = int(float(query_value))
                             
                             if self.compiled_pattern_for_real_values.match(query_value):
                                 query_values.append(query_value)
@@ -79,7 +98,7 @@ class SQLServerOperations(BaseOperations):
                         insert_query = self.insert_query_template.format(table_name, insert_query_columns, query_values_appended)
                         insert_queries.append((insert_query, timeid, countryid))
 
-                    self.insert_new_records(insert_queries)
+                    self.insert_new_records(insert_queries, table_name)
             except Exception as ex:
                 print("An error occurred. Details: " + str(ex))
         else:
